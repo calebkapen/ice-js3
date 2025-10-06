f9019a95dfbc17a64ee25b798c37d748bb64bb23
calebkapen <93950223+calebkapen@users.noreply.github.com>
2025-10-06 18:20:01 -0500

    wrote solution

--- ex6.js
+++ ex6.js
@@ -4,3 +4,8 @@ function rollDie() {
 
 // write a while loop that rolls the die until a 6 is rolled!
 // use console.log to print the roll in every iteration
+let rollResult = 0;
+while (rollResult != 6) {
+  rollResult = rollDie();
+  console.log(rollResult);
+}
